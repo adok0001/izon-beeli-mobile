bd307371f759630661dfe06fa15fb7b2822d2bc4
Tamara Adokeme <digidonado@yahoo.com>
2026-06-05 04:34:30 -0700

    fix: use Xcode 26 image (Apple requirement) and fix Podfile plugin syntax

--- mobile/plugins/withResourceBundleSigning.js
+++ mobile/plugins/withResourceBundleSigning.js
@@ -1,7 +1,16 @@
 const { withDangerousMod } = require("@expo/config-plugins");
-const { mergeContents } = require("@expo/config-plugins/build/utils/generateCode");
 const path = require("path");
 const fs = require("fs");
+
+const PATCH = `
+    installer.pods_project.targets.each do |target|
+      if target.respond_to?(:product_type) && target.product_type == "com.apple.product-type.bundle"
+        target.build_configurations.each do |config|
+          config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
+        end
+      end
+    end
+`;
 
 module.exports = function withResourceBundleSigning(config) {
   return withDangerousMod(config, [
@@ -10,25 +19,14 @@ module.exports = function withResourceBundleSigning(config) {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, "Podfile");
       let podfile = fs.readFileSync(podfilePath, "utf8");
 
-      const snippet = `
-    installer.pods_project.targets.each do |target|
-      if target.respond_to?(:product_type) && target.product_type == "com.apple.product-type.bundle"
-        target.build_configurations.each do |config|
-          config.build_settings['CODE_SIGNING_ALLOWED'] = 'NO'
-        end
-      end
-    end`;
+      if (!podfile.includes("CODE_SIGNING_ALLOWED")) {
+        podfile = podfile.replace(
+          /(\s*react_native_post_install\([\s\S]*?\))\s*\n(\s*end\s*\nend)/,
+          `$1\n${PATCH}\n  $2`
+        );
+        fs.writeFileSync(podfilePath, podfile);
+      }
 
-      podfile = mergeContents({
-        tag: "withResourceBundleSigning",
-        src: podfile,
-        newSrc: snippet,
-        anchor: /react_native_post_install\(/,
-        offset: 5,
-        comment: "#",
-      }).contents;
-
-      fs.writeFileSync(podfilePath, podfile);
       return config;
     },
   ]);
